f5938308f6408d328a7f8e85b98e5ac2a44a2ac9
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-23 19:35:29 -0600

    Move dependencies to be required below rspec/rails [#19]

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,4 @@
-require 'rspec/rails'
 require 'spec_helper'
-require 'webmock/rspec'
-require 'support/factory_bot'
-require 'shoulda/matchers'
 
 
 ENV['RAILS_ENV'] ||= 'test'
@@ -10,6 +6,9 @@ require File.expand_path('../../config/environment', __FILE__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'webmock/rspec'
+require 'support/factory_bot'
+require 'shoulda/matchers'
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -27,7 +26,7 @@ end
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  
+
   config.use_transactional_fixtures = true
 
   config.infer_spec_type_from_file_location!
