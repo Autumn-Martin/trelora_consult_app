67ff66319d3ec6ad29bfee46b2bc1cb876542fee
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-25 20:22:15 -0600

    Add prepare route & controller with index action [#25]

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/find', to: 'properties#show'
+  get '/prepare', to: 'prepare#index'
 
 end
