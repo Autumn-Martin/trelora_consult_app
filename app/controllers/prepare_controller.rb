9a9d878032c821b5282377ce49bad4a1a98561bb
ClaireBvs <clairembeauvais@gmail.com>
2018-10-26 21:19:55 -0600

    add PrepareController with api call to get address data [#27]

--- app/controllers/prepare_controller.rb
+++ app/controllers/prepare_controller.rb
@@ -1,5 +1,29 @@
-class PrepareController < ApplicationController
-  def index
+require "net/http"
+require "uri"
 
+class PrepareController < ApplicationController
+  def show
+    token = session[:user_token]
+    address = session[:addresses]
+
+    params = {address: address[0],
+      HTTP_AUTH_TOKEN: token}
+    params = URI.encode_www_form(params)
+
+    address_uri = URI.parse("https://www.trylora.com/api/v0/turing/properties?#{params}")
+    http = Net::HTTP.new(address_uri.host, address_uri.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(address_uri.request_uri)
+    request['Accept'] = 'application/json'
+    request['Content-Type'] = 'application/json'
+    response.body = http.request(request)
+
+    # getting_response
+    response = http.request(Net::HTTP::Get.new(address_uri.request_uri))
+
+    address_data = JSON.parse(response.body)['result']
+
+    @address = Address.new(address_data)
   end
 end
--- app/controllers/properties_controller.rb
+++
@@ -1,7 +0,0 @@
-class PropertiesController < ApplicationController
-
-  def show
-
-  end
-
-end
