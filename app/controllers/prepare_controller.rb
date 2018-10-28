f761b794a7f298d8586dd3bfcf55b42cc74990e5
abdyqew <aqudrat630@gmail,com>
2018-10-28 14:42:40 -0600

    adds functionality and sad paths for find property

--- app/controllers/prepare_controller.rb
+++ app/controllers/prepare_controller.rb
@@ -4,10 +4,15 @@ require "uri"
 class PrepareController < ApplicationController
   def show
     token = session[:user_token]
-    defined_params = {address: params["q"],
-      HTTP_AUTH_TOKEN: token}
-    
-    defined_params = URI.encode_www_form(defined_params)
+
+    unless params["q"] == ""
+      new_params = {address: params["q"],
+        HTTP_AUTH_TOKEN: token}
+    else
+      new_params = {address: params["new_q"],
+        HTTP_AUTH_TOKEN: token}
+    end
+    defined_params = URI.encode_www_form(new_params)
 
     address_uri = URI.parse("https://www.trylora.com/api/v0/turing/properties?#{defined_params}")
     http = Net::HTTP.new(address_uri.host, address_uri.port)
@@ -20,10 +25,12 @@ class PrepareController < ApplicationController
 
     # getting_response
     response = http.request(Net::HTTP::Get.new(address_uri.request_uri))
-
-    address_data = JSON.parse(response.body)['result']
-
-    @address = Address.new(address_data)
+    if JSON.parse(response.body)['result'].nil?
+      flash[:notice] = "Address Not Found"
+      redirect_to '/find'
+    else
+      address_data = JSON.parse(response.body)['result']
+      @address = Address.new(address_data)
+    end
   end
 end
-
