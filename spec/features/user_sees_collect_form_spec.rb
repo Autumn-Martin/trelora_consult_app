5477e6ec20086043c2de72172f9614f7c21174b7
ClaireBvs <clairembeauvais@gmail.com>
2018-10-28 16:02:47 -0600

    add test user fills collect form [#35]

--- spec/features/user_sees_collect_form_spec.rb
+++ spec/features/user_sees_collect_form_spec.rb
@@ -14,6 +14,19 @@ feature 'A user' do
     expect(page).to have_field(:credit_card_expiration_date)
     expect(page).to have_button("Save")
     expect(page).to have_button("Save and Finish")
+  end
 
+  scenario 'fills the collect form' do
+    visit '/collect'
+
+    fill_in :about_the_home, with: 'Very spacious'
+    fill_in :recommended_list_price, with: 250900
+    select 'Hot', from: :update_client_enthusiasm
+    fill_in :buyer_agent_commission, with: '2%'
+    fill_in :about_the_seller, with: 'Buying new condo'
+    fill_in :credit_card_number, with: "****_****_****_****"
+    fill_in :credit_card_expiration_date, with: 'Nov 22'
+
+    click_on "Save"
   end
 end
