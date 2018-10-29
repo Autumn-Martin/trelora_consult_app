30e7a2055cfc4b3eea62e95a4b5ad7f6e575054b
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-29 17:35:54 -0600

    Add test for user clicking on save and finish on collect form [#35]

--- spec/features/user_sees_collect_form_spec.rb
+++ spec/features/user_sees_collect_form_spec.rb
@@ -35,16 +35,57 @@ feature 'A user' do
 
     visit '/collect'
 
-    fill_in :about_the_home, with: 'Very spacious'
-    fill_in :recommended_list_price, with: 250900
-    select 'Hot', from: :update_client_enthusiasm
-    fill_in :buyer_agent_commission, with: '2%'
-    fill_in :about_the_seller, with: 'Buying new condo'
-    fill_in :credit_card_number, with: "****_****_****_****"
-    fill_in :credit_card_expiration_date, with: 'Nov 22'
+    fill_in :about_the_home, with: 'Spectacular'
+    # fill_in :recommended_list_price, with: '250900'
+    select 'Mild', from: :update_client_enthusiasm
+    fill_in :buyer_agent_commission, with: '78%'
+    fill_in :about_the_seller, with: 'Buying spaceship'
+    fill_in :credit_card_number, with: '****_****_****_**42'
+    fill_in :credit_card_expiration_date, with: 'Nov 50'
 
-    click_on "Save"
+    click_on 'Save'
 
     expect(current_path).to eq('/collect')
+    expect(find_field(:about_the_home).value).to eq('Spectacular')
+    expect(find_field(:buyer_agent_commission).value).to eq('78%')
+    expect(find_field(:about_the_seller).value).to eq('Buying spaceship')
+    expect(find_field(:credit_card_number).value).to eq('****_****_****_**42')
+    expect(find_field(:credit_card_expiration_date).value).to eq('Nov 50')
+  end
+  scenario 'fills the collect form' do
+    visit '/login'
+
+    email = 'steven@trel.co'
+    password = 'password'
+
+    fill_in :email, with: email
+    fill_in :password, with: password
+    click_on "Login"
+
+    expect(current_path).to eq( "/find")
+
+    select "1860 South Marion Street Denver CO 80210", from: :q
+    click_on "Find Location"
+
+    expect(current_path).to eq("/prepare")
+
+    visit '/collect'
+
+    fill_in :about_the_home, with: 'Spectacular'
+    # fill_in :recommended_list_price, with: '250900'
+    select 'Mild', from: :update_client_enthusiasm
+    fill_in :buyer_agent_commission, with: '78%'
+    fill_in :about_the_seller, with: 'Buying spaceship'
+    fill_in :credit_card_number, with: '****_****_****_****'
+    fill_in :credit_card_expiration_date, with: 'Nov 50'
+
+    click_on 'Save and Finish'
+
+    expect(current_path).to eq(success_path)
+    expect(page).to have_content('Spectacular')
+    expect(page).to have_content('78%')
+    expect(page).to have_content('Buying spaceship')
+    expect(page).to have_content('****_****_****_****')
+    expect(page).to have_content('Nov 50')
   end
 end
