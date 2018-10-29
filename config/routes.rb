370215604f459864e6bb5beff1da1d585a8f3296
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-29 17:43:12 -0600

    Add route for success page that will display updated info [#35]

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get '/prepare', to: 'prepare#show'
   get '/collect', to: 'collect#new'
   post '/collect', to: 'collect#create'
+  get '/success', to: 'collect#success'
 end
