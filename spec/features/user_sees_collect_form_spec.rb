49168b31c06309b3c27edfb903ccfd3f49c1d3b1
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-29 18:46:43 -0600

    Change test value of list price to fall within constraint [#35]

--- spec/features/user_sees_collect_form_spec.rb
+++ spec/features/user_sees_collect_form_spec.rb
@@ -72,7 +72,7 @@ feature 'A user' do
     visit '/collect'
 
     fill_in :about_the_home, with: 'Wooowie spectacular'
-    fill_in :recommended_list_price, with: '100000000'
+    fill_in :recommended_list_price, with: '99999999'
     select 'Mild', from: :update_client_enthusiasm
     fill_in :buyer_agent_commission, with: '78%'
     fill_in :about_the_seller, with: 'Buying spaceship'
@@ -84,7 +84,7 @@ feature 'A user' do
     expect(current_path).to eq(success_path)
     expect(page).to have_content('Wooowie spectacular')
     expect(page).to have_content('78%')
-    expect(page).to have_content('100000000')
+    expect(page).to have_content('99999999')
     expect(page).to have_content('Buying spaceship')
     expect(page).to have_content('****_****_****_****')
     expect(page).to have_content('Nov 50')
