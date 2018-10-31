499031928c61c2310cf0d559f8153567d1ad2c02
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-31 05:20:46 -0600

    Add styling for prepare page; adjust fields & instance variables [#53]

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,5 @@
 class Address
-  attr_reader :street, :city, :state, :zip, :name, :email, :phone, :opted_in, :found_us, :zillow_estimate, :zillow_low_estimation, :zillow_high_estimation, :home_junction_estimate, :home_junction_low_estimation, :home_junction_high_estimation, :enthusiasm, :timing, :adopter_type, :stage, :baths, :beds
+  attr_reader :street, :city, :state, :zip, :name, :email, :phone, :opted_in, :found_us, :zillow_estimate, :zillow_low_estimation, :zillow_high_estimation, :home_junction_regional_average, :home_junction_low_estimation, :home_junction_high_estimation, :enthusiasm, :timing, :adopter_type, :stage, :baths, :beds
 
   def initialize(data)
     @street = data['listing']['address']['address1']
@@ -14,9 +14,9 @@ class Address
     @zillow_estimate = data['pricing_estimates']['zillow']['zestimate']
     @zillow_low_estimation = data['pricing_estimates']['zillow']['low']
     @zillow_high_estimation = data['pricing_estimates']['zillow']['high']
-    @home_junction_estimate = data['pricing_estimates']['zillow']['zestimate']
-    @home_junction_low_estimation = data['pricing_estimates']['zillow']['low']
-    @home_junction_high_estimation = data['pricing_estimates']['zillow']['high']
+    @home_junction_regional_average = data['pricing_estimates']['home_junction']['regional_avg']
+    @home_junction_low_estimation = data['pricing_estimates']['home_junction']['low']
+    @home_junction_high_estimation = data['pricing_estimates']['home_junction']['high']
     @enthusiasm = data['client']['enthusiasm']
     @timing = data['client']['timing']
     @adopter_type = data['client']['adopter_type']
