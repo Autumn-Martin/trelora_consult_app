a8186fe0608958e3e58fc5fc7c6b71b82d556b49
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-31 07:13:16 -0600

    Add access to exterior and interior updates [#53]

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,13 @@
 class Address
-  attr_reader :street, :city, :state, :zip, :name, :email, :phone, :opted_in, :found_us, :zillow_estimate, :zillow_low_estimation, :zillow_high_estimation, :home_junction_regional_average, :home_junction_low_estimation, :home_junction_high_estimation, :enthusiasm, :timing, :adopter_type, :stage, :baths, :beds
+  attr_reader :street, :city, :state, :zip, :ext_updates, :int_updates, :name, :email, :phone, :opted_in, :found_us, :zillow_estimate, :zillow_low_estimation, :zillow_high_estimation, :home_junction_regional_average, :home_junction_low_estimation, :home_junction_high_estimation, :enthusiasm, :timing, :adopter_type, :stage, :baths, :beds
 
   def initialize(data)
     @street = data['listing']['address']['address1']
     @city = data['listing']['address']['city']
     @state = data['listing']['address']['state']
     @zip = data['listing']['address']['zip']
+    @ext_updates = data['listing']['home_updates']['exterior']
+    @int_updates = data['listing']['home_updates']['interior']
     @name = data['client']['client_info']['name']
     @email = data['client']['client_info']['email']
     @phone = data['client']['client_info']['phone']
