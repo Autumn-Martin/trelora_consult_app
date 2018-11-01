6b873b5a206c44307faed0400c5ba8a670300829
abdyqew <aqudrat630@gmail,com>
2018-10-31 18:33:45 -0600

    adds field nil css color change for all fields in collect form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,21 +25,55 @@
 
 $(document).ready(function() {
   $('[data-button]').on('click', function () {
-    var InputElement = $('#about_the_home')
+    var HomeInputElement = $('#about_the_home')
+    var ListPriceInputElement = $('#recommended_list_price')
+    var CommissionInputElement = $('#buyer_agent_commission')
+    var SellerInputElement = $('#about_the_seller')
+    var CCInputElement = $('#credit_card_number')
+    var CCExpInputElement = $('#credit_card_expiration_date')
     var submitButtonElement = $('[data-button]')
 
-    const invalidMessage = "Please fill in the field."
+    const invalidMessage = "Please fill in the required fields for submission"
 
     function setInvalidCSS(element) {
       $(element).css({"background-color": "red"});
+    }
+
+    function setRequiredField(element) {
+      $(element).required(true);
     }
 
     function setMessage(message) {
       $('.message').text(message);
     }
 
-    if (InputElement.val() === '') {
-      setInvalidCSS(InputElement)
+    if (HomeInputElement.val() === '') {
+      setInvalidCSS(HomeInputElement)
+      setMessage(invalidMessage)
+      event.preventDefault();
+    } else {}
+    if (ListPriceInputElement.val() === '') {
+      setInvalidCSS(ListPriceInputElement)
+      setMessage(invalidMessage)
+      event.preventDefault();
+    } else {}
+    if (CommissionInputElement.val() === '') {
+      setInvalidCSS(CommissionInputElement)
+      setMessage(invalidMessage)
+      event.preventDefault();
+    } else {}
+    if (SellerInputElement.val() === '') {
+      setInvalidCSS(SellerInputElement)
+      setMessage(invalidMessage)
+      event.preventDefault();
+    } else {}
+    if (CCInputElement.val() === '') {
+      setInvalidCSS(CCInputElement)
+      setMessage(invalidMessage)
+      event.preventDefault();
+    } else {}
+    if (CCExpInputElement.val() === '') {
+      setInvalidCSS(CCExpInputElement)
       setMessage(invalidMessage)
       event.preventDefault();
     } else {}
