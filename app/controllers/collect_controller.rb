be9359198fb6e3a4fef94ffea730db5d7692b1d3
ClaireBvs <clairembeauvais@gmail.com>
2018-11-01 19:24:13 -0600

    Refactor CollectController to add TreloraService [#69]

--- app/controllers/collect_controller.rb
+++ app/controllers/collect_controller.rb
@@ -1,53 +1,39 @@
 require "net/http"
 require "uri"
 class CollectController < ApplicationController
-
   def new
   end
 
   def create
+    service = TreloraService.new(collect_params)
     if params[:save]
-      update_call
+      service.update_listing_information(token, email, address)
       render :new
     elsif params[:save_and_finish]
-      updated = update_call
+      updated = service.update_listing_information(token, email, address)
       redirect_to action: :success, updated: updated
     end
-  end
-
-  def update_call
-    token = session[:user_token]
-    email = session[:email]
-    address = session[:choosen_address]
-
-    update_uri = URI.parse("https://www.trylora.com/api/v0/turing/update_listing_consultation")
-
-    http = Net::HTTP.new(update_uri.host, update_uri.port)
-    http.use_ssl = true
-
-    post_request = Net::HTTP::Post.new(update_uri.request_uri)
-
-    post_request['Accept'] = 'application/json'
-    post_request['Content-Type'] = 'application/json'
-    update_attributes = { HTTP_AUTH_TOKEN: token,
-                          email: email,
-                          address: address,
-                          about_this_home: params[:about_the_home],
-                          list_price: params[:recommended_list_price],
-                          client_enthusiasm: params[:update_client_enthusiasm],
-                          commission: params[:buyer_agent_commission],
-                          about_the_seller: params[:about_the_seller],
-                          credit_card: params[:credit_card_number],
-                          exp_date: params[:credit_card_expiration_date]
-                         }
-    post_request.set_form_data(update_attributes)
-    response = http.request(post_request)
-
-    update_listing = JSON.parse(response.body)
   end
 
   def success
     @updated = params["updated"]["listing_consultation"]
   end
 
+  private
+
+  def collect_params
+    params.permit(:about_the_home, :recommended_list_price, :update_client_enthusiasm, :buyer_agent_commission, :about_the_seller, :credit_card_number, :credit_card_expiration_date)
+  end
+
+  def token
+    session[:user_token]
+  end
+
+  def email
+    session[:email]
+  end
+
+  def address
+    session[:choosen_address]
+  end
 end
