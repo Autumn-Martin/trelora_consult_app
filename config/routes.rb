7bef5a30b0eabecd33a63961406212312106e6b9
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-11-01 19:24:37 -0600

    adds test and route for logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'sessions#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   get '/find', to: 'find#show'
   get '/prepare', to: 'prepare#show'
   get '/collect', to: 'collect#new'
--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -48,4 +48,34 @@ feature 'user login' do
     expect(current_path).to eq('/login')
     expect(page).to have_content('Member Email or Password Incorrect.')
   end
+  
+  scenario 'user can logout after finishing listing' do
+    email = 'steven@trel.co'
+    password = 'password'
+
+    visit '/login'
+
+    fill_in :email, with: email
+    fill_in :password, with: password
+    click_on 'Login'
+    
+    select '1860 South Marion Street Denver CO 80210', from: :q
+    click_on 'Find Location'
+    click_on 'Begin Visit'
+    
+    fill_in :about_the_home, with: 'Spectacular'
+    select 'Mild', from: :client_enthusiasm
+    fill_in :buyer_agent_commission, with: '78%'
+    fill_in :about_the_seller, with: 'Buying spaceship'
+    fill_in :credit_card_number, with: '****_****_****_**42'
+    fill_in :credit_card_expiration_date, with: 'Nov 30'
+    
+    click_on 'Save and Finish'
+
+    expect(current_path).to eq('/success')
+    click_on 'Logout'
+    
+    expect(current_path).to eq('/login')
+    expect(page).to have_content('You have been successfully logged out!')
+  end
 end
