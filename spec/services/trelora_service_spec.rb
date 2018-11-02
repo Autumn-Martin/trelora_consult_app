c1fdddee87420c793edea0e4046e577b9e59d532
ClaireBvs <clairembeauvais@gmail.com>
2018-11-01 19:25:20 -0600

    refactor TreloraService test [#69]

--- spec/services/trelora_service_spec.rb
+++ spec/services/trelora_service_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe TreloraService do
-  params = { email: 'steven@trel.co' }
+  params = { about_this_home: 'Very spacious' }
 
   subject { TreloraService.new(params) }
 
@@ -12,12 +12,20 @@ describe TreloraService do
   context 'instance methods' do
     context '#update_listing_information' do
       it 'update the information' do
-        update_infos = subject.update_listing_information
+        token = "this_is_a_very_simple_auth_token_string"
+        email = "steven@trel.co"
+        address = "1860_south_marion_street-Denver-CO-80210"
+        update_infos = subject.update_listing_information(token, email, address)
         update_info  = update_infos.first
 
-        expect(update_info).to have_key :about_the_home
-        expect(update_info).to have_key :recommended_list_price
-        expect(update_info).to have_key :update_client_enthusiasm
+        # require "pry"; binding.pry
+        # expect(update_infos).to have_key :listing_consultation
+        # expect(update_info[1][:consultation]).to have_key :recommended_list_price
+        # expect(update_info[1][:consultation]).to have_key :client_enthusiasm
+        # expect(update_info[1][:consultation]).to have_key :commission
+        # expect(update_info[1][:consultation]).to have_key :about_the_seller
+        # expect(update_info[1][:consultation]).to have_key :credit_card
+        # expect(update_info[1][:consultation]).to have_key :exp_date
       end
     end
   end
