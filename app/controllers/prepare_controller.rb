618688eee4823ba73279be8c942ab0b4d89cfa03
abdyqew <aqudrat630@gmail,com>
2018-11-01 18:51:53 -0600

    builds property service, deletes code in prepare controller

--- app/controllers/prepare_controller.rb
+++ app/controllers/prepare_controller.rb
@@ -25,24 +25,12 @@ class PrepareController < ApplicationController
       end
     end
     unless new_params == nil
-      defined_params = URI.encode_www_form(new_params)
-
-      address_uri = URI.parse("https://www.trylora.com/api/v0/turing/properties?#{defined_params}")
-      http = Net::HTTP.new(address_uri.host, address_uri.port)
-      http.use_ssl = true
-
-      request = Net::HTTP::Get.new(address_uri.request_uri)
-      request['Accept'] = 'application/json'
-      request['Content-Type'] = 'application/json'
-      response.body = http.request(request)
-
-      # getting_response
-      response = http.request(Net::HTTP::Get.new(address_uri.request_uri))
-      if JSON.parse(response.body)['result'].nil?
+      property_search = Trelora::REST::PropertyService.new(new_params)
+      if property_search.result.nil?
         flash[:notice] = "Address Not Found"
         redirect_to '/find'
       else
-        address_data = JSON.parse(response.body)['result']
+        address_data = property_search.result
         @address = Address.new(address_data)
       end
     else
@@ -65,7 +53,7 @@ class PrepareController < ApplicationController
 
     def fix_manual_input_address(unclean_address)
       words = unclean_address.gsub(/[-_,]/, " ").split
-      if words.count > 3 
+      if words.count > 3
         "#{words.first(4).join("_")}" + "-" + "#{words[-3].capitalize}" + "-" + "#{words[-2].upcase}"+ "-" + "#{words[-1]}"
       end
     end
