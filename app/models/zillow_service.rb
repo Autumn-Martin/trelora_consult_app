2fbf82cfbc5bf689786e74b833914731c9287911
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-01 23:54:19 -0600

    Refactor out methods [#53, #48]

--- app/models/zillow_service.rb
+++ app/models/zillow_service.rb
@@ -11,10 +11,6 @@ class ZillowService
   def retrieve_zillow
     connect
     make_request
-    # xml = http.request(request).body # xml response
-    # doc = Nokogiri::XML(xml) # parse xml doc
-    # homedetails = doc.xpath("//homedetails")[0] # search xml #=> #(Element:0x3fc1fcdcb038 {name = "homedetails", children = [#(Text "https://www.zillow.com/homedetails/1860-S-Marion-St-Denver-CO-80210/13371534_zpid/")]})
-    # homedetails.text #=> https://www.zillow.com/homedetails/1860-S-Marion-St-Denver-CO-80210/13371534_zpid/
   end
 
   private
@@ -26,15 +22,18 @@ class ZillowService
   def make_request
     request['Accept'] = 'application/json'
     request['Content-Type'] = 'application/json'
-    response.body = http.request(request)
     xml = http.request(request).body # xml response
+    extract_web_address(xml)
+  end
+
+  def extract_web_address(xml)
     doc = Nokogiri::XML(xml) # parse xml doc
     homedetails = doc.xpath("//homedetails")[0] # search xml #=> #(Element:0x3fc1fcdcb038 {name = "homedetails", children = [#(Text "https://www.zillow.com/homedetails/1860-S-Marion-St-Denver-CO-80210/13371534_zpid/")]})
     homedetails.text #=> https://www.zillow.com/homedetails/1860-S-Marion-St-Denver-CO-80210/13371534_zpid/
   end
 
   def http
-    Net::HTTP.new(parsed_uri.host, parsed_uri.port)
+    Net::HTTP.new(api_url.host, api_url.port)
   end
 
   def street_address
@@ -46,24 +45,15 @@ class ZillowService
   end
 
   def parsed_uri
-    URI.join("http://www.zillow.com/webservice/", "GetDeepSearchResults.htm?zws-id=X1-ZWz18a4pbgiqyz_493z4&address=1860+South+Marion&citystatezip=Denver%2C+CO%2C+80210")
-    # URI.parse("http://www.zillow.com/webservice/GetDeepSearchResults.htm?#{defined_params}")
+    URI.join("http://www.zillow.com/webservice/", "GetDeepSearchResults.htm?zws-id=X1-ZWz18a4pbgiqyz_493z4&address=#{street_address}&citystatezip=#{city_state}")
   end
 
-  # def defined_params
-  #   params = { :'zws-id' => ENV["zillow_api_key"], # the formatting of zws-id is to escape the dash
-  #              address: street_address,
-  #              citystatezip: city_state,
-  #            }
-  #   URI.encode_www_form(params)
-  # end
-
   def request
-    Net::HTTP::Get.new(parsed_uri.request_uri)
+    Net::HTTP::Get.new(api_url.request_uri)
   end
 
   def response
-    http.request(Net::HTTP::Get.new(parsed_uri.request_uri))
+    http.request(Net::HTTP::Get.new(api_url.request_uri))
   end
 
 end
