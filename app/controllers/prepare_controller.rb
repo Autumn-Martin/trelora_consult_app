63ec3205ef1ab0542dc3ff756b575edfb0af72d1
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-02 00:44:56 -0600

    Remove methods that have been moved to AddressCleaner [#53]

--- app/controllers/prepare_controller.rb
+++ app/controllers/prepare_controller.rb
@@ -16,30 +16,11 @@ class PrepareController < ApplicationController
       flash[:notice] = "Address Not Found"
       redirect_to '/find'
     end
-    
+
     if params["q"] != ""
       session[:choosen_address] = params[:q]
     elsif params["new_q"] != ""
       session[:choosen_address] = params[:new_q]
     end
   end
-
-  private
-    def format_address(unclean_address)
-      address_array = unclean_address.gsub(/[-_]/, " ").split
-      address_array.map do |word|
-        if word.length <= 2
-          word.upcase
-        else
-          word.capitalize
-        end
-      end.join(" ")
-    end
-
-    def fix_manual_input_address(unclean_address)
-      words = unclean_address.gsub(/[-_,]/, " ").split
-      if words.count > 3
-        "#{words.first(4).join("_")}" + "-" + "#{words[-3].capitalize}" + "-" + "#{words[-2].upcase}"+ "-" + "#{words[-1]}"
-      end
-    end
 end
